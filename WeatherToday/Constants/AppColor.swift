f53973c29ac0e4f5c1a2ab9ab68c96bd0cd77d23
Alaattin Bedir <alaattin.bedir@intertech.com.tr>
2022-06-11 23:44:04 +0300

    Amend

---
+++ WeatherToday/Constants/AppColor.swift
@@ -0,0 +1,59 @@
+//
+//  AppColor.swift
+//  WeatherToday
+//
+//  Created by Alaattin Bedir on 11.06.2022.
+//
+
+import Foundation
+import UIKit
+
+struct AppColor {
+    static let whiteDefault = UIColor.white
+    static let blackDefault = UIColor.black
+    static let blue = UIColor.mblPeacockBlue
+    static let red = UIColor.mblRed
+    static let vermillion = UIColor.mblVermillion
+    static let mediumGreen = UIColor.mediumGreen
+    static let greyishBrown = UIColor.mblGreyishBrown
+    static let greyish = UIColor.mblGreyish
+    static let disabledGray = UIColor.disabledGray
+    static let black = UIColor.mblBlack
+    
+    static let cerulean = UIColor.mblCerulean
+    static let magenta = UIColor.mblMagenta
+    static let redTwo = UIColor.mblRedTwo
+    static let gradientBlue = UIColor.gradientBlue
+    static let gradientBlueTwo = UIColor.gradientBlueTwo
+    static let orangeRed = UIColor.orangeRed
+    static let darkGreenBlue = UIColor.mblDarkGreenBlue
+    static let ceruleanTwo = UIColor.mblCeruleanTwo
+    static let ceruleanBG = UIColor.mblCeruleanBG
+    static let turquaz = UIColor.mblTurquaz
+    static let mintGreen = UIColor.mblMintGreen
+
+    static let tabbarGray = UIColor(red: 238 / 255, green: 238 / 255, blue: 238 / 255, alpha: 1)
+    static let charcoalGrey = UIColor(red: 55 / 255, green: 71 / 255, blue: 79 / 255, alpha: 1)
+    static let dropShadow = UIColor(red: 1 / 255, green: 132 / 255, blue: 212 / 255, alpha: 0.15)
+
+    static let ceruleanThree = UIColor.mblCeruleanThree
+    static let ceruleanFour = UIColor.mblCeruleanFour
+    static let crtRed = UIColor.crtRed
+    static let battleshipGrey = UIColor.mblBattleshipGrey
+    static let errorMessageBg = UIColor.mblErrorMessageBg
+    static let errorMessageBorder = UIColor.mblErrorMessageBorder
+    static let frogGreen = UIColor.mblFrogGreen
+    static let tableCellHighlightedCellBgColor = UIColor.mblPaleGrey
+    static let pinkishGrey = UIColor.mblPinkishGrey
+    static let lightBlack = UIColor.mblLightBlack
+    static let bgGray = UIColor.mblbgGray
+    static let bgCerulean = UIColor.mblbgCerulean
+    static let bgGreyishBrown = UIColor.mblBGGreyishBrown
+    static let warmGrey = UIColor.mblWarmGrey
+    static let bgMintGray = UIColor.mblMintGreen
+    static let pink = UIColor(red: 1.0, green: 102 / 255, blue: 153 / 255, alpha: 1)
+    static let tomato = UIColor.mblTomato
+    static let veryLightPink = UIColor(red: 241 / 255, green: 241 / 255, blue: 241 / 255, alpha: 1)
+    static let cloudyBlue = UIColor(red: 182 / 255, green: 198 / 255, blue: 215 / 255, alpha: 1)
+    static let blueGrey = UIColor(red: 120 / 255, green: 144 / 255, blue: 156 / 255, alpha: 1.0)
+}
